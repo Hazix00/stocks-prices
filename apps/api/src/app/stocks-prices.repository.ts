1d91c02e5a45ec9cefe587069460456fbdb4f261
hamza <taoussi.hamza.dev@gmail.com>
2023-10-04 22:07:50 +0100

    feat: implement StocksPricesRepository.findStocksPricesGroupedByMonthAndAvgPrice method

--- apps/api/src/app/stocks-prices.repository.ts
+++ apps/api/src/app/stocks-prices.repository.ts
@@ -1,8 +1,12 @@
-import { StockPrice } from '@prisma/client';
+import { Prisma, StockPrice } from '@prisma/client';
 import { PrismaService } from './common/prisma.service';
 import { QueryStocksDto } from './dtos/query-stocks.dto';
+import { StocksPricesPerMonthDto } from '@shared';
+import { BadRequestException, Logger } from '@nestjs/common';
 
 export class StocksPricesRepository extends PrismaService {
+
+  logger = new Logger(StocksPricesRepository.name);
   /**
    * The function `find` retrieves stock prices based on the provided query parameters.
    * @param {QueryStocksDto} query - The `query` parameter is an object of type `QueryStocksDto`. It is
@@ -27,5 +31,67 @@ export class StocksPricesRepository extends PrismaService {
     return this.stockPrice.findMany({
       where: filterQuery,
     });
+  }
+
+  /**
+   * The function `findStocksPricesGroupedByMonthAndAvgPrice` retrieves the average prices of stocks
+   * grouped by month and company, based on the provided year and company filters.
+   * @param {QueryStocksDto} stocksQuery - The `stocksQuery` parameter is an object that contains the
+   * following properties:
+   * 'year': number
+   * 'company': string
+   * @returns a Promise that resolves to an array of objects of type StocksPricesPerMonthDto.
+   */
+  async findStocksPricesGroupedByMonthAndAvgPrice(
+    stocksQuery: QueryStocksDto = {}
+  ): Promise<StocksPricesPerMonthDto[]> {
+    const { year, company } = stocksQuery;
+    let query = `
+      WITH MonthlyStocks AS (
+          SELECT
+            EXTRACT(MONTH FROM TO_TIMESTAMP(timestamp / 1000)) :: INTEGER AS month,
+            EXTRACT(YEAR FROM TO_TIMESTAMP(timestamp / 1000)) :: INTEGER AS year,
+            company,
+            AVG(("lowestPriceOfTheDay" + "highestPriceOfTheDay") / 2) AS avgAvgPriceOfMonth
+          FROM public."${process.env.TABLE}"`;
+
+    if (year) {
+      query += ` WHERE EXTRACT(YEAR FROM TO_TIMESTAMP(timestamp / 1000)) = ${year}`;
+    }
+
+    if (company) {
+      query += year ? ' AND ' : ' WHERE ';
+      query += `company = '${company}'`;
+    }
+
+    query += `
+          GROUP BY
+            company,
+            timestamp
+      )
+      SELECT
+        company,
+        month,
+        AVG(avgAvgPriceOfMonth) AS avgPrice,
+        year
+      FROM
+          MonthlyStocks
+      GROUP BY
+        month,
+        company,
+        year
+      ORDER BY
+          month;
+    `;
+
+    console.log(query);
+
+    try {
+      return await this.$queryRaw(Prisma.raw(query));
+    } catch (error) {
+      this.logger.error(error);
+      throw new BadRequestException(error.message);
+    }
+
   }
 }
