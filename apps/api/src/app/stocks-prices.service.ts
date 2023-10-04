b9febd9dd51d622939ba3ff3c7dbdf0b8a6200ef
hamza <taoussi.hamza.dev@gmail.com>
2023-10-04 22:24:09 +0100

    feat: implement StocksPricesService.findStocksPricesGroupedByMonthAndAvgPrice method

--- apps/api/src/app/stocks-prices.service.ts
+++ apps/api/src/app/stocks-prices.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { StocksPricesRepository } from './stocks-prices.repository';
+import { QueryStocksDto } from './dtos/query-stocks.dto';
 
 @Injectable()
-export class StocksPricesService {}
+export class StocksPricesService {
+  constructor(
+    private readonly stocksPricesRepository: StocksPricesRepository
+  ) {}
+
+  /**
+   * The function "findStocksPricesGroupedByMonthAndAvgPrice" retrieves stock prices based on a query
+   * and groups them by month, returning the average price for each month.
+   * @param {QueryStocksDto} stocksQuery - The `stocksQuery` parameter is an object of type
+   * `QueryStocksDto` which contains the query parameters for filtering the stocks prices.
+   */
+  findStocksPricesGroupedByMonthAndAvgPrice(
+    stocksQuery: QueryStocksDto = {}
+  ) {
+    return this.stocksPricesRepository.findStocksPricesGroupedByMonthAndAvgPrice(
+      stocksQuery
+    );
+  }
+}
