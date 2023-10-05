6bfd08d5550094ec91c1219cd1ba5cd0d52aa4d3
hamza <taoussi.hamza.dev@gmail.com>
2023-10-05 08:34:46 +0100

    feat: implement StocksPricesService.bestGain method

--- apps/api/src/app/stocks-prices.service.ts
+++ apps/api/src/app/stocks-prices.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { StocksPricesRepository } from './stocks-prices.repository';
 import { QueryStocksDto } from './dtos/query-stocks.dto';
+import { BestGain } from '@shared';
+import { BestGainQueryDto } from './dtos/best-gain-query.dto';
 
 @Injectable()
 export class StocksPricesService {
@@ -14,11 +16,54 @@ export class StocksPricesService {
    * @param {QueryStocksDto} stocksQuery - The `stocksQuery` parameter is an object of type
    * `QueryStocksDto` which contains the query parameters for filtering the stocks prices.
    */
-  findStocksPricesGroupedByMonthAndAvgPrice(
-    stocksQuery: QueryStocksDto = {}
-  ) {
+  findStocksPricesGroupedByMonthAndAvgPrice(stocksQuery: QueryStocksDto = {}) {
     return this.stocksPricesRepository.findStocksPricesGroupedByMonthAndAvgPrice(
       stocksQuery
     );
+  }
+
+  async bestGainOfTheYear(bestGainQuery: BestGainQueryDto): Promise<BestGain> {
+    const stockData = await this.stocksPricesRepository.find(bestGainQuery);
+
+    if (!stockData || stockData.length < 2) {
+      throw new InternalServerErrorException(
+        'Insufficient data to find best buy and sell days.'
+      );
+    }
+
+    let maxGain =
+      stockData[0].highestPriceOfTheDay - stockData[0].lowestPriceOfTheDay;
+    if (maxGain < 0) {
+      maxGain = 0;
+    }
+    let buyDayIndex = 0;
+    let sellDayIndex = 0;
+
+    // Iterate through the stock data to find the best buy and sell days
+    for (let buyIndex = 0; buyIndex < stockData.length - 1; buyIndex++) {
+      for (
+        let sellIndex = buyIndex + 1;
+        sellIndex < stockData.length;
+        sellIndex++
+      ) {
+        const buyPrice = stockData[buyIndex].lowestPriceOfTheDay;
+        const sellPrice = stockData[sellIndex].highestPriceOfTheDay;
+        const potentialGain = sellPrice - buyPrice;
+
+        if (potentialGain > maxGain) {
+          maxGain = potentialGain;
+          buyDayIndex = buyIndex;
+          sellDayIndex = sellIndex;
+        }
+      }
+    }
+    console.log('maxGain', maxGain);
+
+    // Cast the timestamps to Number to avoid JSON.stringify() error : TypeError: Do not know how to serialize a BigInt
+    stockData[buyDayIndex].timestamp = Number(stockData[buyDayIndex].timestamp) as unknown as bigint;
+    stockData[sellDayIndex].timestamp = Number(stockData[sellDayIndex].timestamp) as unknown as bigint;
+
+    // Return the best buy and sell days
+    return { buyDay: stockData[buyDayIndex], sellDay: stockData[sellDayIndex] };
   }
 }
