050c3205a26fa07ddf7c9e93f9f5c760698631ce
hamza <taoussi.hamza.dev@gmail.com>
2023-10-05 08:53:47 +0100

    test: StocksPricesController.bestGainOfTheYear

--- apps/api/src/app/stocks-prices.controller.spec.ts
+++ apps/api/src/app/stocks-prices.controller.spec.ts
@@ -3,6 +3,11 @@ import { StocksPricesController } from './stocks-prices.controller';
 import { StocksPricesService } from './stocks-prices.service';
 import { stocksPricesPerMonthStub } from '../test/stubs/stocks-prices-per-month.stubs';
 import { QueryStocksDto } from './dtos/query-stocks.dto';
+import { BestGainQueryDto } from './dtos/best-gain-query.dto';
+import { async } from 'rxjs';
+import { stocksPricesStub } from '../test/stubs/stocks-prices.stubs';
+import { BestGain } from '@shared';
+import { InternalServerErrorException } from '@nestjs/common';
 
 describe('StocksPricesController', () => {
   let controller: StocksPricesController;
@@ -16,6 +21,7 @@ describe('StocksPricesController', () => {
           provide: StocksPricesService,
           useValue: {
             findStocksPricesGroupedByMonthAndAvgPrice: jest.fn(),
+            bestGainOfTheYear: jest.fn(),
           },
         },
       ],
@@ -112,6 +118,58 @@ describe('StocksPricesController', () => {
       ).toHaveBeenCalledWith(query);
 
       expect(result).toEqual(stubs);
+    });
+  });
+
+  describe('when calling bestGainOfTheYear', () => {
+    const mockQuery = {
+      year: 2022,
+      company: 'GOOGLE',
+    };
+    it('then should be defined', () => {
+      expect(controller.bestGainOfTheYear).toBeDefined();
+    });
+
+    it('return the best buy and sell days', async () => {
+      // Mock
+      const mockFindResult = stocksPricesStub().filter(
+        (item) => item.company === mockQuery.company
+      );
+      const mockResult: BestGain = {
+        buyDay: mockFindResult[0],
+        sellDay: mockFindResult[1],
+      };
+
+      jest.spyOn(service, 'bestGainOfTheYear').mockResolvedValue(mockResult);
+
+      // Call
+      const result = await controller.bestGainOfTheYear(mockQuery);
+
+      // Assert
+      expect(service.bestGainOfTheYear).toHaveBeenCalledTimes(1);
+      expect(service.bestGainOfTheYear).toHaveBeenCalledWith(mockQuery);
+
+      expect(result).toEqual(mockResult);
+    });
+
+    it('then throw an error if no data is found', async () => {
+      jest
+        .spyOn(service, 'bestGainOfTheYear')
+        .mockRejectedValue(
+          new InternalServerErrorException(
+            'Insufficient data to find best buy and sell days.'
+          )
+        );
+      // Call
+      try {
+        await controller.bestGainOfTheYear(mockQuery);
+      }
+      catch (error) {
+        // Assert
+        expect(service.bestGainOfTheYear).toHaveBeenCalledTimes(1);
+        expect(service.bestGainOfTheYear).toHaveBeenCalledWith(mockQuery);
+        expect(error).toBeInstanceOf(InternalServerErrorException);
+      }
     });
   });
 });
